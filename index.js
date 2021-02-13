5ec257961b9cc7da568366f7f191cba8164205c5
franco <franco@localhost.localdomain>
2021-02-13 01:23:21 -0300

    se separan los archivos del front y del backend para subirlos a heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require("mongoose");
 var app = require('./app');
-var port = process.env.PORT || 3999;
+var port = process.env.PORT || 8550;
 
 mongoose.set("useFindAndModify", false);
 mongoose.Promise = global.Promise;
@@ -14,7 +14,7 @@ mongoose
   .then(() => {
     console.log("la coneccion a mongo se realizo");
     app.listen(port, () => {
-      console.log("El servidor http://localhost:3999 está funcionando !!!");
+      console.log("El servidor http://localhost:8550 está funcionando !!!");
     });
   })
   .catch((e) => console.log(e));
--- quasar-sisvet/src/boot/axios.js
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import axios from "axios";
-
-Vue.prototype.$axios = axios;
-// ^ ^ ^ this will allow you to use this.$axios
-//       so you won't necessarily have to import axios in each vue file
