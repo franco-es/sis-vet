849d8350c000e07c30cdad7c6f22ebc69b475659
Elio Togno <elio.etogno@gmail.com>
2023-05-20 12:31:40 -0300

    02 - second fixes

---
+++ routes/ownerRoutes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+const ownerRouter = Router();
+import { OwnerController } from "../controllers/owner.js";
+import { validate_owner } from "../middlewares/validateData.js";
+import { authenticated } from "../middlewares/authenticated.js";
+
+ownerRouter.post(
+  "/new",
+  [authenticated, validate_owner],
+  new OwnerController.save
+);
+ownerRouter.put(
+  "/update",
+  [authenticated, validate_owner],
+  new OwnerController.save
+);
+ownerRouter.get("/getOwner", authenticated, new OwnerController.getOwner);
+
+export { ownerRouter };
